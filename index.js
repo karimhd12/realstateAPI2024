c772184e8f94ef4cd130acb17ad89f85eb3fa532
karimgaber12 <karimgaberhd@gmail.com>
2024-07-17 19:26:37 +0300

    hgfhf

--- index.js
+++ index.js
@@ -47,3 +47,10 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
